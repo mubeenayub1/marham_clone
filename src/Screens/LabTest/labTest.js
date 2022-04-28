3e303df01e6682d7d7c843d794ae11948118419b
Mubeen Ayub <mubeenayub1608@gmail.com>
2022-04-28 04:39:14 -0700

    picture update

--- src/Screens/LabTest/labTest.js
+++ src/Screens/LabTest/labTest.js
@@ -52,10 +52,330 @@ const LabTest = ({navigation}) => {
           <Icon name="search1" type="antdesign" color={'grey'} />
         </Pressable>
       </View>
-
-      <View style={styles.cardContainer}>
-        <View style={styles.imageContainer}></View>
-      </View>
+      <ScrollView
+        contentContainerStyle={{marginBottom: 50}}
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Chughtai Lab</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:284</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>15% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Dr. Essa's Laboratory</Text>
+              <Text style={styles.detailText}>8:00 AM - 8:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:20</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>15% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Citilab and Rearch Center</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:72</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>CLINILAB</Text>
+              <Text style={styles.detailText}>12:00 AM - 12:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:3</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>One Health Lab</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:7</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>15% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Alnoor Diagnostic Center</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:1</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Capital Medical Lab</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:1</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>10% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Mughal Diagnostic Lab</Text>
+              <Text style={styles.detailText}>8:00 AM - 8:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:20</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Metropole Laboratories</Text>
+              <Text style={styles.detailText}>8:00 AM - 8:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:1</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>25% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Pride Lab</Text>
+              <Text style={styles.detailText}>9:00 AM - 9:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:1</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.offtextcontainer}>
+            <Text style={styles.offtext}>20% off</Text>
+          </View>
+          <View style={styles.wholeContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require('../../assets/lab.png')}
+                style={styles.cardimage}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.headingText}>Pro Lab Diagnostic Center</Text>
+              <Text style={styles.detailText}>8:00 AM - 11:00 PM</Text>
+              <Text style={styles.detailText}>Available branches:1</Text>
+            </View>
+          </View>
+          <View style={styles.wholeButtonContainer}>
+            <Pressable
+              style={[styles.buttonContainer, {backgroundColor: 'white'}]}>
+              <Text style={[styles.buttonText, {color: '#014e78'}]}>
+                View Profile
+              </Text>
+            </Pressable>
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Select Tests</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={{height: 30}}></View>
+      </ScrollView>
     </View>
   );
 };
